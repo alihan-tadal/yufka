b58fc2adb2779229cd6e35b2a677dbdcddc52dba
alihan-tadal <tadal.alihan@gmail.com>
2023-09-18 15:31:02 +0300

    Complete protocol file.

---
+++ test/test_client.py
@@ -0,0 +1,8 @@
+import unittest
+
+from . import no_logging
+from yufka.client import Piece, Block
+
+
+class PieceTests(unittest.TestCase):
+    pass
--- yufka/client.py
+++ yufka/client.py
@@ -1,7 +1,26 @@
+import asyncio
+import logging
+import math
+import os
+import time
+from asyncio import Queue
+from collections import namedtuple, defaultdict
+from hashlib import sha1
+
+from yufka.protocol import Peerconnection, RUQUEST_SIZE
+from yufka.tracker import Tracker
+
+MAX_PEER_CONNECTIONS = 40
+
 class TorrentClient:
     def __init__(self, torrent):
-        print(torrent)
-        pass
-
+        self.tracker = Tracker(torrent)
+        self.available_peers = Queue()
+        self.peers = []
+        self.piece_manager = PieceManager(torrent)
+        self.abort = False
+     
+    async def start(self):
+        self.peers = [PeerConnection(self.available_peers, 
     def stop(self):
         pass
